b1238e5b47692340efaecc7814fb8dc72f5683f8
meerocodes <amirest1997@gmail.com>
2023-03-06 17:47:32 -0500

    made the workout return card flip over while clearing the innerHTML. added Devins assets folder

--- scripts/app.js
+++ scripts/app.js
@@ -56,7 +56,20 @@ submitButton.addEventListener('click', function(event){
       }
 
     function printWorkout(){
-        for (let i = 0; i < numWorkoutValue; i++) {
+
+
+        // Create a new array to store unique workouts
+  // const uniqueWorkouts = [];
+
+  //       // Loop through userWorkout array and add unique workouts to the uniqueWorkouts array
+  //       for (let i = 0; i < userWorkout.length; i++) {
+  //         const workout = userWorkout[i];
+  //         if (!uniqueWorkouts.some((w) => w.name === workout.name)) {
+  //           uniqueWorkouts.push(workout);
+  //         }
+  //       }
+ 
+         for (let i = 0; i < numWorkoutValue; i++) {
                 const individualWorkout = userWorkout[i];
                 //console.log(individualWorkout);
                 const newListItem = document.createElement('li');
@@ -126,3 +139,31 @@ console.log(userWorkout)
 
 
 });
+const ulParent = document.querySelector('#workoutReturn');
+const originalContent = {};
+
+ulParent.addEventListener('click', function(event) {
+  if (event.target.tagName === 'LI') {
+    // Check if original content has been stored for this li element
+    console.log(event)
+    const liId = event.target.getAttribute('id');
+    if (!originalContent[liId]) {
+      originalContent[liId] = event.target.innerHTML;
+    }
+
+    // Toggle the flip-card class
+    event.target.classList.toggle('flip-card');
+
+    // Check if flip-card class has been removed
+    if (!event.target.classList.contains('flip-card')) {
+      // Restore original content
+      event.target.innerHTML = originalContent[liId];
+    } else {
+      // Blank out the content
+      event.target.innerHTML = '';
+    }
+  }
+});
+
+
+
